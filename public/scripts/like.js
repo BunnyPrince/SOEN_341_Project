2b870f1ca8cc5df1e6c45a16540cbb702844f490
eyeshield2110 <ndinh@hotmail.fr>
2021-04-03 19:07:23 -0400

    #107 : Frontend test for like-unlike-button

--- public/scripts/like.js
+++ public/scripts/like.js
@@ -3,70 +3,102 @@ function setVisibilityLike(img) {
     document.getElementById('unlike-button-' + img).style.display = 'inline-block'
 }
 
-function editLike(img, type) {
+function editLike(img, type, mockDoc) {
+    // load virtual document
+    let doc
+    if (typeof document === 'undefined') {
+        doc = mockDoc
+    } else {
+        doc = document
+    }
     // iframe
-    let iframe = document.createElement('iframe');
-    iframe.setAttribute('name', 'like-iframe');
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
+    let iframe = doc.createElement('iframe')
+    iframe.setAttribute('name', 'like-iframe')
+    iframe.style.display = 'none'
+    doc.body.appendChild(iframe)
     // form
-    let form = document.createElement('form');
-    form.setAttribute('method', 'post');
-    form.setAttribute('action', '/' + type + '?_method=PUT');
-    form.setAttribute('target', 'like-iframe');
+    let form = doc.createElement('form')
+    form.setAttribute('method', 'post')
+    form.setAttribute('action', '/' + type + '?_method=PUT')
+    form.setAttribute('target', 'like-iframe')
     form.style.display = 'none'
-    let input = document.createElement('input');
-    input.setAttribute('name', 'image');
-    input.setAttribute('value', img);
+    let input = doc.createElement('input')
+    input.setAttribute('name', 'image')
+    input.setAttribute('value', img)
     input.style.display = 'none'
-    form.style.display = 'none';
-    form.appendChild(input);
-    document.body.appendChild(form);
+    form.style.display = 'none'
+    form.appendChild(input)
+    doc.body.appendChild(form)
     if (type === 'like') {
-        document.getElementById('like-button-' + img).style.display = 'none'
-        document.getElementById('unlike-button-' + img).style.display = 'inline-block'
+        doc.getElementById('like-button-' + img).style.display = 'none'
+        doc.getElementById('unlike-button-' + img).style.display = 'inline-block'
+        /*
         try {
             updateLike(img, true)
         } catch (e) {
         } finally {
             form.submit()
         }
+        */
+        updateLike(img, true, mockDoc)
+        //form.submit()
     } else {
-        document.getElementById('unlike-button-' + img).style.display = 'none'
-        document.getElementById('like-button-' + img).style.display = 'inline-block'
+        doc.getElementById('unlike-button-' + img).style.display = 'none'
+        doc.getElementById('like-button-' + img).style.display = 'inline-block'
+        /*
         try {
             updateLike(img, false)
         } catch (e) {
         } finally {
             form.submit()
         }
+        */
+        updateLike(img, false, mockDoc)
+        //form.submit()
+    }
+    if (typeof mockDoc === 'undefined')
+        form.submit()
+    return {
+        formAction: form.getAttribute('action'),
+        inputValue: input.getAttribute('value')
     }
 }
 
 
-function updateLike(img, inc) {
-    if (inc) {
-        showCurrentUserInList(img)
-        let i = parseInt(document.getElementById('like-number-' + img).innerText)
-        document.getElementById('like-number-' + img).innerText = (i + 1) + ''
+function updateLike(img, inc, mockDoc) {
+    let doc
+    if (typeof document === 'undefined') {
+        doc = mockDoc
     } else {
-        hideCurrentUserInList(img)
-        let i = parseInt(document.getElementById('like-number-' + img).innerText)
-        document.getElementById('like-number-' + img).innerText = (i - 1) + ''
+        doc = document
+    }
+    if (inc) {
+        let show = doc.getElementById('current-user-in-' + img)
+        show.style.display = 'block'
+        // showCurrentUserInList(img)
+        let i = parseInt(doc.getElementById('like-number-' + img).innerText)
+        doc.getElementById('like-number-' + img).innerText = (i + 1) + ''
+    } else {
+        let hide = doc.getElementById('current-user-in-' + img)
+        hide.style.display = 'none'
+        // hideCurrentUserInList(img)
+        let i = parseInt(doc.getElementById('like-number-' + img).innerText)
+        doc.getElementById('like-number-' + img).innerText = (i - 1) + ''
     }
 }
 
 function showCurrentUserInList(img) {
-    console.log('img id:', img)
+    //console.log('img id:', img)
     let show = document.getElementById('current-user-in-' + img)
-    console.log(show)
+    // console.log(show)
     show.style.display = 'block'
 }
 
 function hideCurrentUserInList(img) {
-    console.log('img id:', img)
+    //console.log('img id:', img)
     let hide = document.getElementById('current-user-in-' + img)
-    console.log(hide)
-        hide.style.display = 'none'
+    //console.log(hide)
+    hide.style.display = 'none'
 }
 
+module.exports = editLike
