bfb6896e7caeadd0ddbdbd905fe0045ed05909c9
BunnyPrince <jananee1999@yahoo.com>
2021-04-14 22:21:13 -0400

    #111 check all the ejs and css files to see if the codding style are the same. All so check if there is any duplicated classes

--- public/scripts/image.js
+++ public/scripts/image.js
@@ -1,14 +1,14 @@
 function showPreviewOne(event){
       if(event.target.files.length > 0){
-        let src = URL.createObjectURL(event.target.files[0]);
-        let preview = document.getElementById("file");
-        preview.src = src;
-        preview.style.display = "block";
+        let src = URL.createObjectURL(event.target.files[0])
+        let preview = document.getElementById("file")
+        preview.src = src
+        preview.style.display = "block"
           return true;
       }
       return false;
     }
     function myImgRemoveFunctionOne() {
-      document.getElementById("file").src = "https://i.ibb.co/ZVFsg37/default.png";
+      document.getElementById("file").src = "https://i.ibb.co/ZVFsg37/default.png"
       return true;
     }
