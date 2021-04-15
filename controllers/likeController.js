7d743a4da3606783114987a627daf782420ca962
eyeshield2110 <ndinh@hotmail.fr>
2021-04-15 12:46:43 -0400

    #111 Finished last test for `deleteImages` (business logic function in `services`>`imgServices.js`)

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -3,12 +3,10 @@ const {like, unlike} = require('../services/likeServices')
 
 const profileLikeImage = async (req, res) => {
     const image = await like(req, Image)
-    console.log('liked')
 }
 
 const profileUnlikeImage = async (req, res) => {
     const image = await unlike(req, Image)
-    console.log("unliked")
 }
 
 module.exports = {
